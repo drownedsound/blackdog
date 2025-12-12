a677f9bdf5933dec3f031792adc277b103953488
Drowned Sound <carlo.c.jacinto@gmail.com>
2025-12-12 21:07:51 +0800

    Added todo

--- internal/features/application/repository.go
+++ internal/features/application/repository.go
@@ -11,6 +11,7 @@ var (
 	ErrConnectionRefused = errors.New("database connection refused")
 )
 
+// FIXME: Rename to DataProvider to be consistent with Go naming standards
 type Repository interface {
 	Save(ctx context.Context, a *Application) error
 	GetById(ctx context.Context, id int64) (Application, error)
